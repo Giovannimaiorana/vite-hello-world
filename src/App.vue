bd432267e614235b2d41775a4c39b7be5a22283f
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-03-24 16:09:39 +0100

    fine esercixio + bonus

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import titolo from './components/titolo.vue'
+import jumbrotron from './components/jumbotron.vue'
 export default {
   components: {
     titolo,
+    jumbrotron
   }
 }
 </script>
 
 <template>
   <titolo />
+  <jumbrotron />
 </template>
 
 <style></style>
---
+++ src/components/jumbotron.vue
@@ -0,0 +1,22 @@
+<script></script>
+
+<template>
+    <div class="containerJumbo">
+        <h1>Sembra quasi magia !!</h1>
+    </div>
+</template>
+
+<style>
+.containerJumbo {
+    width: 100%;
+    height: 150px;
+    background-color: beige;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h1 {
+    text-align: center;
+}
+</style>
